6d32a42cb48f9e8017d85827e62469ee3c0c3b99
Ruhyat Ma'rullah <ruhyatmarullah@gmail.com>
2023-05-21 22:09:28 +0700

    task day 4 clear

--- script/blog.js
+++ script/blog.js
@@ -1,10 +1,4 @@
 let dataBlog = []
-let tech = []
-
-for (let k = 0; k < tech.length; k++) {
-    const element = tech[k];
-    
-}
 
 function addBlog(event) {
     event.preventDefault();
@@ -12,12 +6,25 @@ function addBlog(event) {
     let technology = []
 
     let title = document.getElementById("input-title").value
-    let startDate = document.getElementById("input-start-date").value
-    let endDate = document.getElementById("input-end-date").value
+    let startDate = new Date(document.getElementById("input-start-date").value) 
+    let endDate = new Date(document.getElementById("input-end-date").value) 
     let description = document.getElementById("input-description").value
     let image = document.getElementById("input-image").files
     let checkbox = document.getElementsByName("technology")
-
+     
+    if (title == '') {
+        return alert("title can't null");
+    } else if (startDate == 'Invalid Date') {
+        return alert('take your date in Start Date')
+    } else if (endDate == 'Invalid Date') {
+        return alert('take your date in End Date')
+    } else if ( description == '') {
+        return alert("Description can't null")
+    }
+    
+    let months = new Date(endDate - startDate)
+    let durasi = months.getMonth()
+    
     for (let index = 0; index < checkbox.length; index++) {       
         if (checkbox[index].checked) {
             technology.push(checkbox[index].value);
@@ -25,7 +32,7 @@ function addBlog(event) {
     }
 
     image = URL.createObjectURL(image[0])
-    console.log(image);
+
 
     let blog = {
         title,
@@ -33,19 +40,18 @@ function addBlog(event) {
         endDate,
         description,
         technology,
-        image
+        image,
+        durasi
     }
-    
-    tech.push(blog.technology)
     dataBlog.push(blog)
-    console.log(dataBlog);
-    console.log(tech);
-    renderBlog(event);
-
+    renderBlog(event)
 }
+
 
 function renderBlog(event) {
     event.preventDefault()
+
+    document.getElementById("myForm").reset()
 
     document.getElementById("content").innerHTML = ""
     
@@ -64,7 +70,7 @@ function renderBlog(event) {
                 </div>
                 <div class="project-card-title">
                     <h2>${dataBlog[index].title}</h2>
-                    <p>Durasi: ${dataBlog[index].startDate} to ${dataBlog[index].endDate}</p>
+                    <p>Durasi: ${dataBlog[index].durasi} Bulan</p>
                 </div>
                 <div class="project-card-description">
                     <p>${dataBlog[index].description}</p>
@@ -80,3 +86,4 @@ function renderBlog(event) {
         `
     }
 }
+

74709183dcbd87ed2aa77ee979cd0b4809f81766
Ruhyat Ma'rullah <ruhyatmarullah@gmail.com>
2023-05-23 22:00:31 +0700

    task day 5 finish

--- script/blog.js
+++ script/blog.js
@@ -1,77 +1,73 @@
-let dataBlog = []
+let dataBlog = [];
 
 function addBlog(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let technology = []
+  let technology = [];
 
-    let title = document.getElementById("input-title").value
-    let startDate = new Date(document.getElementById("input-start-date").value) 
-    let endDate = new Date(document.getElementById("input-end-date").value) 
-    let description = document.getElementById("input-description").value
-    let image = document.getElementById("input-image").files
-    let checkbox = document.getElementsByName("technology")
-     
-    if (title == '') {
-        return alert("title can't null");
-    } else if (startDate == 'Invalid Date') {
-        return alert('take your date in Start Date')
-    } else if (endDate == 'Invalid Date') {
-        return alert('take your date in End Date')
-    } else if ( description == '') {
-        return alert("Description can't null")
+  let title = document.getElementById("input-title").value;
+  let startDate = new Date(document.getElementById("input-start-date").value);
+  let endDate = new Date(document.getElementById("input-end-date").value);
+  let description = document.getElementById("input-description").value;
+  let image = document.getElementById("input-image").files;
+  let checkbox = document.getElementsByName("technology");
+
+  console.log(startDate);
+
+  if (title == "") {
+    return alert("title can't null");
+  } else if (startDate == "Invalid Date") {
+    return alert("take your date in Start Date");
+  } else if (endDate == "Invalid Date") {
+    return alert("take your date in End Date");
+  } else if (description == "") {
+    return alert("Description can't null");
+  }
+
+  for (let index = 0; index < checkbox.length; index++) {
+    if (checkbox[index].checked) {
+      technology.push(checkbox[index].value);
     }
-    
-    let months = new Date(endDate - startDate)
-    let durasi = months.getMonth()
-    
-    for (let index = 0; index < checkbox.length; index++) {       
-        if (checkbox[index].checked) {
-            technology.push(checkbox[index].value);
-        }
-    }
+  }
 
-    image = URL.createObjectURL(image[0])
+  image = URL.createObjectURL(image[0]);
 
+  let blog = {
+    title,
+    startDate,
+    endDate,
+    description,
+    technology,
+    image,
+  };
 
-    let blog = {
-        title,
-        startDate,
-        endDate,
-        description,
-        technology,
-        image,
-        durasi
-    }
-    dataBlog.push(blog)
-    renderBlog(event)
+  dataBlog.push(blog);
+  renderBlog();
 }
 
+function renderBlog() {
+  document.getElementById("myForm").reset();
+  document.getElementById("content").innerHTML = "";
 
-function renderBlog(event) {
-    event.preventDefault()
+  for (let index = 0; index < dataBlog.length; index++) {
+    let tech = "";
 
-    document.getElementById("myForm").reset()
+    for (let k = 0; k < dataBlog[index].technology.length; k++) {
+      tech += `<img src="assets/images/${dataBlog[index].technology[k]}.png" alt=""></img>`;
+    }
 
-    document.getElementById("content").innerHTML = ""
-    
-    for (let index = 0; index < dataBlog.length; index++) {
-        let tech = ''
-
-        for (let k = 0; k < dataBlog[index].technology.length; k++) {
-            tech += `<img src="assets/images/${dataBlog[index].technology[k]}.png" alt=""></img>`
-        }
-        
-        document.getElementById("content").innerHTML += 
-        `
+    document.getElementById("content").innerHTML += `
         <a href="blog-detail.html">
-            <div class="project-card">
+          <div class="project-card">
                 <div class="project-card-image">
                     <img src="${dataBlog[index].image}" alt="blog_image">
                 </div>
                 <div class="project-card-title">
                     <h2>${dataBlog[index].title}</h2>
-                    <p>Durasi: ${dataBlog[index].durasi} Bulan</p>
+                    <p>Durasi: ${getDistanceTime(
+                      dataBlog[index].startDate,
+                      dataBlog[index].endDate
+                    )}</p>
                 </div>
                 <div class="project-card-description">
                     <p>${dataBlog[index].description}</p>
@@ -84,9 +80,40 @@ function renderBlog(event) {
                     <Button>delete</Button>
                 </div>
             </div>
-        
         </a>
-        `
-    }
+        `;
+  }
 }
 
+function getDistanceTime(startTime, endTime) {
+  let distance = endTime - startTime;
+
+  let milisecond = 1000;
+  let secondInHours = 3600;
+  let hoursInDays = 24;
+  let daysInWeeks = 7;
+
+  let distanceYears = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays * 365)
+  );
+  console.log(distanceYears);
+  let distanceMonths = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays * 30)
+  );
+  let distanceWeeks = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays * daysInWeeks)
+  );
+  let distanceDay = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDays)
+  );
+
+  if (distanceDay < 7) {
+    return `${distanceDay} Days`;
+  } else if (distanceDay < 30) {
+    return `${distanceWeeks} Weeks`;
+  } else if (distanceDay < 365) {
+    return `${distanceMonths} Months`;
+  } else {
+    return `${distanceYears} Years`;
+  }
+}
